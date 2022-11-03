a6642d71560bc730880e44948f0cb55cbd791c98
VascoMiguens <v_miguens@hotmail.com>
2022-11-03 13:03:26 +0000

    - display forecast for the next  5 days - create header for the forecast - for loop to iterate through 5 items of the object returned by the api - customized the forecast cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,12 @@ var cityForm = document.querySelector("#city-form");
 var cityInput = document.querySelector("#city-input");
 var weatherSect = document.querySelector("#weather-section");
 var weatherCurr = document.querySelector("#current-weather");
+var forecastCont = document.querySelector("#forecast");
+var search = document.querySelector("#recent-section");
 var citySearched;
 var formatDay = { day: "numeric", month: "numeric", year: "numeric" };
+
+var userSearch = JSON.parse(localStorage.getItem("search")) || [];
 
 var WEATHER_BASE_API_URL = "https://api.openweathermap.org/";
 var WEATHER_API_KEY = "c828ba608f878d909ac2c8a1fd874157";
@@ -54,11 +58,13 @@ var findCoords = function (search) {
 var displayCityandCountry = function (weatherData) {
   //create h2 element that will holde city and country
   citySearched = document.createElement("h2");
+  citySearched.style.color = "#e2dfd2";
   citySearched.innerHTML = `${weatherData.name}, ${weatherData.country}`;
   //append the city and country to the section
   weatherSect.appendChild(citySearched);
   //cal function to get object with weather details and pass longitude and latitude
   getWeatherDetails(weatherData.lat, weatherData.lon);
+  saveEntry(weatherData.name);
 };
 
 //get weather details and call functions to display current weather
@@ -69,21 +75,19 @@ var getWeatherDetails = function (lat, lon) {
     .then((response) => response.json())
     .then((data) => {
       displayCurrentWeather(data);
+
+      displayForecast(data);
     });
 };
 
 var displayCurrentWeather = function (currWeather) {
-  //if the object retuned is empty, display message
-  if (currWeather === 0) {
-    weatherSect.textContent = "No city found";
-    return;
-  }
-  //formata the hour
+  var today = currWeather.current;
+  //format the hour
   var fomatHour = { hour: "numeric", minute: "numeric" };
   //convert UNIX time to human readble time
-  var tempUNIX = new Date(currWeather.current.dt * 1000);
-  var sunrise = new Date(currWeather.current.sunrise * 1000);
-  var sunset = new Date(currWeather.current.sunset * 1000);
+  var tempUNIX = new Date(today.dt * 1000);
+  var sunrise = new Date(today.sunrise * 1000);
+  var sunset = new Date(today.sunset * 1000);
   //format the time returned
   var tempTime = tempUNIX.toLocaleDateString("en-GB", formatDay);
   var tempSunrise = sunrise.toLocaleTimeString("en-GB", fomatHour);
@@ -93,27 +97,76 @@ var displayCurrentWeather = function (currWeather) {
   currDateEl.textContent = " " + "(" + tempTime + ")";
   //append the current date to city and country
   citySearched.appendChild(currDateEl);
+  weatherSect.style.backgroundColor = "#005a9c";
   //create a div element that will display the current day temperature and details
   weatherCurr.innerHTML = `
        <div id="temperature" class="col-4">
-        <img id="icon" src="https://openweathermap.org/img/w/${currWeather.current.weather[0].icon}.png"></img>
-          <h2 id="temperature_value">${currWeather.current.temp}°C</h2>
-          <p id="feels-like">Feels like: ${currWeather.current.feels_like}°C</p>
-          <span id="description_value">${currWeather.current.weather[0].description}</span>
+        <img id="icon" src="https://openweathermap.org/img/w/${today.weather[0].icon}.png"></img>
+          <h3 id="temperature_value">${today.temp}°C</h3>
+          <p id="feels-like">Feels like: ${today.feels_like}°C</p>
+          <span id="description_value">${today.weather[0].description}</span>
           
       </div>
       <div id="temperature-details" class="col-8"">
           <div>Sunrise: ${tempSunrise}</div>
           <div>Sunset: ${tempSunset}</div>
-          <div>Wind: ${currWeather.current.wind_speed} MPS</div>
-          <div>Clouds: ${currWeather.current.clouds}%</div>
-          <div>Visibility: ${currWeather.current.visibility} m</div>
-          <div>Humidity: ${currWeather.current.humidity}%</div>
-          <div>Dew Point: ${currWeather.current.dew_point} C</div>
-          <div>UV Index: ${currWeather.current.uvi} nm</div>
+          <div>Wind: ${today.wind_speed} MPS</div>
+          <div>Clouds: ${today.clouds}%</div>
+          <div>Visibility: ${today.visibility} m</div>
+          <div>Humidity: ${today.humidity}%</div>
+          <div>Dew Point: ${today.dew_point} C</div>
+          <div>UV Index: ${today.uvi} nm</div>
         </div>
   `;
   weatherSect.appendChild(weatherCurr);
 };
+
+var displayForecast = function (forecast) {
+  //pass the object retrieved and concatenate desired path to variable
+  var dayWeather = forecast.daily;
+  //create header element
+  var forecastHeader = document.createElement("h4");
+  forecastHeader.textContent = "5 Day Forecast";
+  forecastHeader.style.cssText =
+    "font-weight: 900;border-top: 3px solid #e2dfd2; padding-top: 20px";
+  //append elements
+  weatherSect.appendChild(forecastHeader);
+  weatherSect.appendChild(forecastCont);
+  forecastCont.innerHTML = "";
+  //First element of the retrieved object is already displayed so start at index 1 and return the next 5
+  for (var i = 1; i < 6; i++) {
+    var daily = dayWeather[i];
+    var day = new Date(daily.dt * 1000).toLocaleDateString("en-GB", formatDay);
+    var newDay = document.createElement("div");
+    newDay.innerHTML = `
+      <div class="weather-card">
+        <div class="date">
+          <span>${day}</span>
+        </div>
+        <div id="icon">
+        <img class="align-center" src="https://openweathermap.org/img/w/${daily.weather[0].icon}.png">
+        </div>
+        <div class="temperature">
+          <span>Temp: ${daily.temp.day}°C</span>
+        </div>
+        <div class="tem-max">
+        <span>Max: ${daily.temp.max}°C</span>
+        </div>
+        <div class="temp-min">
+        <span>Min: ${daily.temp.min}°C</span>
+        </div>
+        <div class="wind">
+        <span>Wind: ${daily.wind_speed} KPS</span>
+        </div>
+        <div class="humidity">
+          <span>Hum: ${daily.humidity}%
+          </span>
+        </div>
+    `;
+    forecastCont.appendChild(newDay);
+  }
+};
+
+
 
 cityForm.addEventListener("submit", formSubmitHandler);
